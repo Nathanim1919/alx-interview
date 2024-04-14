5dc31c5e992fbdfcaa528b72c7f190b0e4ed1bda
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-14 22:35:03 +0300

    Pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    """Prints Pascal's triangle with n rows."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    for row in triangle:
+        print(row)
+
+    return triangle
