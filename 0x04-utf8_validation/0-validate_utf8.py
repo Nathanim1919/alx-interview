4f9315d532f00349481a14ca400a5180d96eaf15
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-09 23:16:05 +0300

    utf-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,5 +3,22 @@
 
 
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding."""
-    return all((b & 128) == 0 for b in data)
+    num_bytes_to_follow = 0
+
+    for byte in data:
+        if num_bytes_to_follow == 0:
+            if byte >> 7 == 0:  # Single-byte character
+                continue
+            elif byte >> 5 == 0b110:  # Two-byte character
+                num_bytes_to_follow = 1
+            elif byte >> 4 == 0b1110:  # Three-byte character
+                num_bytes_to_follow = 2
+            elif byte >> 3 == 0b11110:  # Four-byte character
+                num_bytes_to_follow = 3
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                num_bytes_to_follow -= 1
+
+    return num_bytes_to_follow == 0
