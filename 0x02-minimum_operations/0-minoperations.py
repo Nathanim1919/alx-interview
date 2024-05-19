98122a37e0208a13028302ae3e899894e9b56bf0
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-19 13:36:46 +0300

    Implement minOperation problem from ALX Interview Project

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A method that calculates the fewest number of operations
+    needed to result in exactly `n` `H` characters in the file
+
+    Args:
+        n: Length of characters
+    return: An integer
+"""
+
+
+def minOperations(n: int) -> int:
+    operation = 0
+    factor = 2
+    count_outer_loop = 0
+    count_inner_loop = 0
+
+    if (n < 2):
+        return 0
+
+    while (n > 1):
+        while (n % factor == 0):
+            operation += factor
+            n //= factor
+        factor += 1
+    return operation
