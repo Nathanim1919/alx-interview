cfecefc474b82fc7adc82cdbb31a180f8ee4a211
Nathanim1919 <nathanim2tadele@gmail.com>
2024-06-09 23:03:08 +0300

    solve making chnage problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,14 +21,13 @@ def makeChange(coins, total):
     # Initialize dp array
     dp = [0] * (total + 1)
 
-    # Initialize the first element of the dp array
-    dp[0] = 0
-
     # Initialize the rest of the dp array
     for i in range(1, total + 1):
         dp[i] = float('inf')
         for j in range(len(coins)):
             if (coins[j] <= i):
                 dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+                if (dp[i] == 1):
+                    break
 
     return dp[total]
