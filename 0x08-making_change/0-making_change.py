246d53579650f9d31a19a08b7b09bf7eecb6e31e
Nathanim1919 <nathanim2tadele@gmail.com>
2024-06-09 22:43:37 +0300

    implement coin change problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module for making change
+"""
+
+
+def makeChange(coins, total):
+    """this function makes change for a
+    given total using the least amount of coins
+
+    Args:
+        coins (List[number]): list of coins
+        total (number): the total to make change for
+
+    Returns:
+        number: the least amount of coins needed to make change
+    """
+    if (total <= 0):
+        return 0
+
+    # Initialize dp array
+    dp = [0] * (total + 1)
+
+    # Initialize the first element of the dp array
+    dp[0] = 0
+
+    # Initialize the rest of the dp array
+    for i in range(1, total + 1):
+        dp[i] = float('inf')
+        for j in range(len(coins)):
+            if (coins[j] <= i):
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+
+    return dp[total]
