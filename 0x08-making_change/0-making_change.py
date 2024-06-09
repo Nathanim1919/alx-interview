557c71c9329ee639250dd6b6229f04d76b5013b4
Nathanim1919 <nathanim2tadele@gmail.com>
2024-06-09 23:13:13 +0300

    fix effeciency of the implementation

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -26,9 +26,9 @@ def makeChange(coins, total):
 
     # Initialize the rest of the dp array
     for i in range(1, total + 1):
-        for j in range(len(coins)):
-            if (coins[j] <= i):
-                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+        for coin in coins:
+            if (coin <= i):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
                 if (dp[i] == 1):
                     break
 
